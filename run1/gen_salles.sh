a7ad8bdd9253710b63e98d34b126fb8b0dab4c1d
jj_z600 <degiy@yahoo.fr>
2019-06-06 01:02:25 +0200

    bug fix sur non prise systematique du premier match d une poule pour preferences

---
+++ run1/gen_salles.sh
@@ -0,0 +1,9 @@
+rm -f ./t_*.csv
+
+./creneaux_salle.pl -s bouin -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 1
+./creneaux_salle.pl -s roure -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 2
+./creneaux_salle.pl -s ext1  -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 3
+./creneaux_salle.pl -s ext2  -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 4
+./creneaux_salle.pl -s ext3  -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 5
+./creneaux_salle.pl -s ext4  -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 6
+#./creneaux_salle.pl -s ext5  -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 7
