d2b10d27dfa6da3fc8b9b54066ccb3eb30c701e6
jj_z600 <degiy@yahoo.fr>
2019-06-07 01:04:55 +0200

    corrections

--- run1/gen_salles.sh
+++ run1/gen_salles.sh
@@ -1,9 +1,9 @@
 rm -f ./t_*.csv
 
-./creneaux_salle.pl -s bouin -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 1
-./creneaux_salle.pl -s roure -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 2
-./creneaux_salle.pl -s ext1  -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 3
-./creneaux_salle.pl -s ext2  -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 4
-./creneaux_salle.pl -s ext3  -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 5
-./creneaux_salle.pl -s ext4  -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 6
-#./creneaux_salle.pl -s ext5  -p 25 -d 9:30 -f 12:00 -t -t -d 14:00 -f 16:00 -prio 7
+./creneaux_salle.pl -s bouin -p 20 -d 9:40 -f 12:20 -t -t -d 13:40 -f 15:40 -prio 1
+./creneaux_salle.pl -s roure -p 20 -d 9:40 -f 12:20 -t -t -d 13:40 -f 15:40 -prio 2
+./creneaux_salle.pl -s ext1  -p 20 -d 9:40 -f 12:20 -t -t -d 13:40 -f 15:40 -prio 3
+./creneaux_salle.pl -s ext2  -p 20 -d 9:40 -f 12:20 -t -t -d 13:40 -f 15:40 -prio 4
+./creneaux_salle.pl -s ext3  -p 20 -d 9:40 -f 12:20 -t -t -d 13:40 -f 15:40 -prio 5
+./creneaux_salle.pl -s ext4  -p 20 -d 9:40 -f 12:20 -t -t -d 13:40 -f 15:40 -prio 6
+./creneaux_salle.pl -s ext5  -p 20 -d 9:40 -f 12:20 -t -t -d 13:40 -f 15:40 -prio 7
