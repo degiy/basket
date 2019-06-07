8390885d4e037c27c5694dbfe486278d3dcf95f7
U-MND\jjansou <jjansou@PLEC0NQHPU.mnd.intra.risarm>
2019-06-07 15:06:16 +0200

    nouvelles commandes

--- gen.sh
+++ gen.sh
@@ -2,4 +2,7 @@
 
 ./gen_salles.sh 
 ./gen_poules.sh 
+./affect.pl -sm save.csv -v -o matches_sur_creneaux.csv -c creneaux.csv
+./choix_salles.pl  -i ./matches_sur_creneaux.csv -c creneaux.csv -v -v -v
+
 
