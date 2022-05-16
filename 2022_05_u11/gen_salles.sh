8b1ae44ac79e56f8cc4a64c65acb3266435f3ace
degiy <degiy@yahoo.fr>
2022-05-16 12:39:55 +0200

    ajout tournoi 2022

---
+++ 2022_05_u11/gen_salles.sh
@@ -0,0 +1,7 @@
+rm -f ./t_*.csv
+
+./creneaux_salle.pl -s ext1 -p 20 -d 9:20  -f 15:00 -prio 1
+./creneaux_salle.pl -s ext2 -p 20 -d 9:20  -f 15:00 -prio 2
+./creneaux_salle.pl -s ext3 -p 20 -d 9:20  -f 15:00 -prio 3
+./creneaux_salle.pl -s ext4 -p 20 -d 9:20  -f 15:00 -prio 4
+#./creneaux_salle.pl -s ext5 -p 20 -d 9:20  -f 15:20 -prio 5
---
+++ 2022_05_u9/gen_salles.sh
@@ -0,0 +1,8 @@
+rm -f ./t_*.csv
+
+./creneaux_salle.pl -s b1 -p 20 -d 9:40 -f 12:00 -t -t -d 13:20 -f 15:20 -prio 1
+./creneaux_salle.pl -s b2 -p 20 -d 9:40 -f 12:00 -t -t -d 13:20 -f 15:20 -prio 2
+./creneaux_salle.pl -s b3 -p 20 -d 9:40 -f 12:00 -t -t -d 13:20 -f 15:20 -prio 3
+./creneaux_salle.pl -s r1 -p 20 -d 9:40 -f 12:00 -t -t -d 13:20 -f 15:20 -prio 4
+./creneaux_salle.pl -s r2 -p 20 -d 9:40 -f 12:00 -t -t -d 13:20 -f 15:20 -prio 5
+./creneaux_salle.pl -s r3 -p 20 -d 9:40 -f 12:00 -t -t -d 13:20 -f 15:20 -prio 6
